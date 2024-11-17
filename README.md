ec960af921ef36738c589d9063e4cd75ae3d56d4
Poulkalff <pok@gmail.com>
2024-11-17 14:41:44 +0100

    Completed

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 My attempt to get conky and lua to display system settings,
    inspired/developed from: https://github.com/deeptoaster/conky-rings
+                            https://github.com/mustafaozhan/GoogleIntegratedSystemConky
 
 
 <p align="center">
-  <img src="screenshot.png" width="350" title="screenshot example">
+  <img src="screenshot.png" width="450" title="screenshot example">
 </p>
--- drawGraphics.lua
+++ drawGraphics.lua
@@ -4,9 +4,12 @@ config = {
   network_ethernet = 'enp7s0'
 }
 
-disks_rings = {
-  { command = 'mem',  max = 'memmax' },
-  { command = 'swap', max = 'swapmax' }
+disk_rings = {
+  { command = 'fs_used /',  max = 'fs_size /' },
+  { command = 'fs_used /home',  max = 'fs_size /home' },
+  { command = 'fs_used /mnt/data',  max = 'fs_size /mnt/data' },
+  { command = 'fs_used /mnt/3tb_hdd',  max = 'fs_size /mnt/3tb_hdd' },
+  { command = 'fs_used /mnt/8tb_hdd',  max = 'fs_size /mnt/8tb_hdd' }
 }
 
 ram_rings = {
@@ -36,7 +39,9 @@ function toPercentage(command)
   local raw = conky_parse(string.format('${%s}', command))
   local category = string.sub(raw, -3)
   local value = string.sub(raw, 0, -4)
-  if category == "GiB" then
+  if category == "TiB" then
+    value = tonumber(value * 1000000)
+  elseif category == "GiB" then
     value = tonumber(value * 1000)
   else
     value = tonumber(value)
@@ -90,7 +95,7 @@ function draw_ring(cairo, x, y, radius, breakpoints, max, ringIndex, ring_width)
   else
     ring_color = 0xffffff
   end
-  cairo_set_source_rgba(cairo, rgba(0xff0000, config.bg_alpha))
+  cairo_set_source_rgba(cairo, rgba(0xaa0000, config.bg_alpha))
   for breakpoint_index in pairs(breakpoints) do
     local breakpoint_angle = angle(breakpoints[breakpoint_index], max)
     if breakpoint_angle > previous_angle then
@@ -130,29 +135,63 @@ function conky_main()			-- MAIN FUNCTION. Called by conky.conf, as "rings", sinc
   ))
   xCenter = math.floor( (conky_window.width / 2) - 20 )
   yCenter = math.floor( conky_window.height / 2 )
-  -- helper lines
+--  diagonal helper lines
 --  draw_line(cairo, 0, 0, 1717, 1082, 0xffffff)
 --  draw_line(cairo, 0, 982, 1567, 0, 0xffffff)
   draw_cpu(xCenter, yCenter)
-  draw_ram(1160, 310)
-
-
-  draw_disk(400, 800)
-
-
-
+  draw_ram(xCenter - 400, yCenter)
+  draw_disk(xCenter + 400, yCenter)
   -- infoBox border
-  draw_rectangle(cairo, 200, 105, 430, 280, 0xffffff)
+--  draw_rectangle(cairo, 200, 105, 430, 280, 0xffffff)
 end
 
 
 function draw_disk(x, y)
-  displayTexts = {}
+  local displayTexts = {}
+  local positions = {y - 40, y - 28, y - 16, y - 4, y + 8}
+  for ring_index in pairs(disk_rings) do
+    local breakpoints = {}
+    local ring = disk_rings[ring_index]
+    local value = toPercentage(ring.command)
+    local max = toPercentage(ring.max)
+    local str1 = string.sub(ring.command, 9)
+    local str2 = conky_parse(string.format('${%s}', ring.max))
+    table.insert(displayTexts, {str1, str2})
+    if value ~= nil then
+      table.insert(breakpoints, value)
+    end
+    draw_ring(
+      cairo,
+      x,
+      y - 53,
+      140 - ring_index * 12,
+      breakpoints,
+      max,
+      ring_index, 
+      10
+    )
+  end
+  -- Write text at center of rings
+  cairo_select_font_face (cairo, "Serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+  cairo_set_font_size(cairo, 12)
+  for _index in pairs(positions) do
+    local yPos = positions[_index]
+    if (_index % 2 == 0) then
+      cairo_set_source_rgba(cairo, rgba(0xa6a6a6, config.bg_alpha))
+    else
+      cairo_set_source_rgba(cairo, rgba(0xffffff, config.bg_alpha))
+    end
+    cairo_move_to(cairo, x - 162, yPos)
+    cairo_show_text(cairo, displayTexts[_index][1])
+    cairo_move_to(cairo, x - 30, yPos)
+    cairo_show_text(cairo, displayTexts[_index][2])
+  end
+  cairo_stroke(cairo)
 end
 
 
 function draw_ram(x, y)
-  displayTexts = {}
+  local displayTexts = {}
   for ring_index in pairs(ram_rings) do
     local breakpoints = {}
     local ring = ram_rings[ring_index]
@@ -199,11 +238,11 @@ function draw_cpu(x, y)
       cairo,
       x,
       y - 53,
-      168 - ring_index * 8,
+      168 - ring_index * 10,
       breakpoints,
       ring.max,
       ring_index,
-      6
+      8
     )
   end
   -- Write text at center of rings
