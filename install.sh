f939df230f7639b536444cf5cd51fd72265a2666
Poulkalff <poulkalff@gmail.com>
2026-08-10 11:28:15 +0200

    added install

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt install conky-all -y
+sudo apt-get install mesa-utils -y
+sudo cp drawGraphics.lua /etc/conky/
+sudo cp conky.conf /etc/conky/
+sudo cp conky.desktop  ~/.config/autostart/
+
+
